f5b1e078804af82b4e334dfa18baf1b4259bcefc
moureen mutinda <maurinemutinda72@gmail.com>
2024-12-19 11:35:44 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+// Function 1: saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function 2: mondayWork
+  const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Function 3: wrapAdjective
+  function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
